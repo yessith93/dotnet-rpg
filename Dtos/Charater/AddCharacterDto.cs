fc1cf4e524ae2a9a968d50e5904c336404337b73
miguel.almanza <miguel.almanza@DDC-CO-MALMANZA>
2021-12-02 08:53:51 -0500

    avance primer dia estudiando el curso de udemy

---
+++ Dtos/Charater/AddCharacterDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Dtos.Charater
+{  
+    {
+        public int Id {get;set;}
+        public string Name {get; set;}="frodo";
+        public int HitPoints {get;set;} = 100;
+        public int Stregnth {get;set;} = 10;
+        public int Defense  {get;set;} = 10;
+        public int intelligence {get;set;} = 10;
+        public RpgClass Class {get;set; }=RpgClass.knight;
+
+    }
+}
